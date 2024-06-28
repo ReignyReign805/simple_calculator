dc6cd1eef936b634d604d67be13a1705724108f2
ReignyReign805 <jowiereign805@gmail.com>
2024-06-28 21:35:24 +0800

    Added Delete button and multiplication symbol display

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-let num1;
-let num2;
-let operator;
+let num1 = "";
+let num2 = "";
+let operator = ""; 
 
+function displayNumber(number){
+    if (operator === ''){
+        num1 += number;
+        document.getElementById('x').innerHTML = num1;
+    } else {
+        num2 += number;
+        document.getElementById('y').innerHTML = num2;
+    }
+}
+
+function setOperator(op){
+    operator = op;
+    document.getElementById('operator_disp').innerText = (op === '*') ? 'x' : op;
+}
 
 function addition(num1, num2){
     return num1 + num2;
@@ -16,6 +30,60 @@ function division(num1, num2){
     return num1 / num2;
 }
 
-function operate(operator, num1, num2){
+function operate(){
+    let result;
+    const number1 = parseFloat(num1);
+    const number2 = parseFloat(num2);
 
+    switch(operator){
+        case '+' :
+            result = addition(number1, number2);
+            break;
+        case '-':
+            result = subtraction(number1, number2);
+            break;
+        case '*':
+            result = multiplication(number1, number2);
+            break;
+        case '/':
+            result = division(number1, number2);
+            break;
+        default: 
+            return;
+    }
+
+    document.getElementById('x').innerText = result;
+    document.getElementById('operator_disp').innerText = '';
+    document.getElementById('y').innerText = '';
+
+    num1 = roundAccurately(result, 15).toString();
+    num2 = '';
+    operator = '';
+}
+
+
+function roundAccurately(num, places) {
+    return parseFloat(Math.round(num + 'e' + places) + 'e-' + places);
+}
+
+function delNum(){
+    if (num2 !== '') {
+        num2 = num2.slice(0, -1);
+        document.getElementById('y').innerText = num2;
+    } else if (operator !== '') {
+        operator = '';
+        document.getElementById('operator_disp').innerText = '';
+    } else {
+        num1 = num1.slice(0, -1);
+        document.getElementById('x').innerText = num1 || '0'; // Display '0' if num1 is empty
+    }
+}
+
+function clearAll() {
+    num1 = '';
+    num2 = '';
+    operator = '';
+    document.getElementById('x').innerText = '0';
+    document.getElementById('operator_disp').innerText = '';
+    document.getElementById('y').innerText = '';
 }
